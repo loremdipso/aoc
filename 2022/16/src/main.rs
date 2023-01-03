2d04dde6770dd8e3a0e3d699d374b5d38a4084be
Michael Adams <mtadams42@gmail.com>
2023-01-02 23:03:45 -0700

    FINALLY FINISHED DAY 16!!!! Not really sure what I did, apart from use arrays in place of hashmaps everywhere and ignoring 0-flow pipes. Oh well.

--- 2022/16/src/main.rs
+++ 2022/16/src/main.rs
@@ -4,7 +4,7 @@
 extern crate lazy_static;
 
 use bit_set::BitSet;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 mod utils;
 use regex::Regex;
 
@@ -18,132 +18,110 @@ fn main() {
     let filename = "sample.txt";
     // let filename = "input.txt";
 
-    // 1401 is too low
     part_1(filename);
     // part_2(filename);
 }
 
 fn part_1(filename: &str) {
-    let (name_map, graph) = get_graph(filename);
-    dbg!(&graph);
-
+    let (graph, name_map, start_pos) = get_graph(filename);
     let reachable_mapping = get_reachable_mapping(&graph);
-    dbg!(&name_map);
-    dbg!(&reachable_mapping);
+    println!("Starting...");
 
-    let mut dead_states = vec![];
-    let mut states = vec![State::new(0, graph.len())];
+    // Don't even consider nodes that have no flow rate
+    let filtered_flow_rates = (0..graph.len())
+        .filter(|i| graph[*i].flow_rate > 0)
+        .collect();
 
-    let max = 20;
-    let mut new_states = Vec::new();
-    for day in 0..max {
-        println!("{} / {}. States: {}", day, max, states.len());
-
-        let num_states = states.len();
-        new_states = Vec::with_capacity(num_states);
-
-        for state in &mut states {
-            // increment the current flow-rate
-            state.total += state.total_flow_rate;
-            let mut did_a_thing = false;
-
-            let current_node = graph.get(state.current_position).unwrap();
-
-            // Open the current node
-            if !state.open.contains(state.current_position) {
-                did_a_thing = true;
-                let mut temp = state.clone();
-                temp.seen_since_last_open.clear();
-                temp.seen_since_last_open.insert(state.current_position);
-                temp.open.insert(state.current_position.to_owned());
-                temp.total_flow_rate += current_node.flow_rate;
-                new_states.push(temp);
-            }
-
-            // look for potential moves at the current position
-            for neighbor in &current_node.edges {
-                // just move there
-                if valid_move(state, *neighbor, &reachable_mapping) {
-                    did_a_thing = true;
-                    let mut temp = state.clone();
-                    temp.current_position = *neighbor;
-                    temp.seen_since_last_open.insert(neighbor.clone());
-                    new_states.push(temp);
-                }
-            }
-
-            if !did_a_thing {
-                // If there's nothing left to do just finish the looping and save the state
-                let mut state = state.clone();
-                state.total += state.total_flow_rate * (max - (day + 1));
-                dead_states.push(state);
-            }
-        }
-
-        // swap it out
-        states = new_states;
-    }
-
-    let max = states.iter().max_by_key(|e| e.total).unwrap();
-    max.print("Max: ", &name_map);
-
-    let max_dead = dead_states.iter().max_by_key(|e| e.total).unwrap();
-    max_dead.print("Max dead: ", &name_map);
+    let state = State::new(start_pos, graph.len());
+    let max = recur(
+        &graph,
+        &reachable_mapping,
+        &filtered_flow_rates,
+        state,
+        0,
+        30,
+    );
+    dbg!(max);
 }
 
-fn get_reachable_mapping(graph: &Vec<Node>) -> HashMap<(usize, usize), BitSet> {
-    let mut mapping: HashMap<(usize, usize), BitSet> = HashMap::new();
-    for current in 0..graph.len() {
-        for neighbor in &graph[current].edges {
-            let reachable = get_reachable(graph, current, *neighbor);
-            mapping.insert((current, *neighbor), reachable);
+fn recur(
+    graph: &Vec<Node>,
+    reachable_mapping: &ReachableMapping,
+    filtered_flow_rates: &Vec<usize>,
+    state: State,
+    current_day: usize,
+    num_days: usize,
+) -> usize {
+    if current_day > num_days {
+        return state.total;
+    }
+
+    let mut max = state.total;
+
+    let num_nodes = graph.len();
+    let current_position = state.current_position;
+    for target_position in filtered_flow_rates {
+        if !state.open.contains(*target_position) {
+            let cost = reachable_mapping[current_position * num_nodes + target_position];
+
+            let node = &graph[*target_position];
+            let new_day = current_day + cost + 1;
+            if node.flow_rate > 0 && new_day < num_days {
+                let mut state = state.clone();
+
+                state.current_position = *target_position;
+                state.open.insert(*target_position);
+                state.total_flow_rate += node.flow_rate;
+                // just add the rest of the days
+                state.total += node.flow_rate * (num_days - new_day);
+
+                let temp_total = recur(
+                    graph,
+                    reachable_mapping,
+                    filtered_flow_rates,
+                    state,
+                    new_day,
+                    num_days,
+                );
+                if temp_total > max {
+                    max = temp_total;
+                }
+            }
+        }
+    }
+
+    return max;
+}
+
+// { CurrentPosition: { OtherPosition: Distance } }
+type ReachableMapping = Vec<usize>;
+fn get_reachable_mapping(graph: &Vec<Node>) -> ReachableMapping {
+    let mut mapping: ReachableMapping = Vec::new();
+    for start in 0..graph.len() {
+        for end in 0..graph.len() {
+            let cost = bfs(graph, start, end);
+            mapping.push(cost);
         }
     }
     return mapping;
 }
 
-fn get_reachable(graph: &Vec<Node>, current: usize, next: usize) -> BitSet {
-    let mut reachable = BitSet::with_capacity(graph.len());
-    reachable.insert(next); // TODO: do we need this?
-
-    let mut seen = HashSet::new();
-    seen.insert(current);
-
+fn bfs(graph: &Vec<Node>, start: usize, end: usize) -> usize {
     let mut stack = VecDeque::new();
-    stack.push_front(next);
-
+    stack.push_back((0, start));
     loop {
-        if stack.is_empty() {
-            break;
+        let (current_cost, position) = stack.pop_front().unwrap();
+        if position == end {
+            return current_cost;
         }
 
-        let next = stack.pop_front().unwrap();
-        for neighbor in &graph[next].edges {
-            if !seen.contains(neighbor) {
-                seen.insert(*neighbor);
-                stack.push_back(*neighbor);
-                reachable.insert(*neighbor);
-            }
+        for neighbor in &graph[position].edges {
+            stack.push_back((current_cost + 1, *neighbor));
         }
     }
-
-    return reachable;
 }
 
-fn valid_move(
-    state: &mut State,
-    neighbor: usize,
-    reachable_mapping: &HashMap<(usize, usize), BitSet>
-) -> bool {
-    if state.seen_since_last_open.contains(neighbor) {
-        return false;
-    }
-
-    let reachable = reachable_mapping.get(&(state.current_position, neighbor)).unwrap();
-    return reachable.iter().any(|bit| !state.open.contains(bit));
-}
-
-fn get_graph(filename: &str) -> (HashMap<usize, String>, Vec<Node>) {
+fn get_graph(filename: &str) -> (Vec<Node>, HashMap<usize, String>, usize) {
     let lines = utils::get_lines::<String>(filename);
 
     // get the basic mapping from string to usize
@@ -181,7 +159,7 @@ fn get_graph(filename: &str) -> (HashMap<usize, String>, Vec<Node>) {
         graph.push(node);
     }
 
-    return (reverse_map, graph);
+    return (graph, reverse_map, *name_map.get("AA").unwrap());
 }
 
 #[derive(Debug)]
@@ -193,7 +171,6 @@ struct Node {
 
 #[derive(Debug, Clone)]
 struct State {
-    seen_since_last_open: BitSet,
     open: BitSet,
     current_position: usize,
     total: usize,
@@ -203,32 +180,10 @@ struct State {
 impl State {
     pub fn new(current_position: usize, capacity: usize) -> Self {
         Self {
-            seen_since_last_open: BitSet::with_capacity(capacity),
             open: BitSet::with_capacity(capacity),
             current_position,
             total: 0,
             total_flow_rate: 0,
         }
-    }
-
-    pub fn print(&self, prefix: &str, name_map: &HashMap<usize, String>) {
-        println!(
-            "{}\n\tCurrent:  {}\n\tTotal: {}\n\tFlow:  {}\n\tOpen:  {}",
-            prefix,
-            name_map.get(&self.current_position).unwrap(),
-            self.total,
-            self.total_flow_rate,
-            Self::get_bitstring(name_map, &self.open)
-        );
-    }
-
-    pub fn get_bitstring(name_map: &HashMap<usize, String>, bits: &BitSet) -> String {
-        let mut rv = Vec::new();
-        for index in 0..bits.capacity() {
-            if bits.contains(index) {
-                rv.push(name_map.get(&index).unwrap().to_owned());
-            }
-        }
-        return rv.join(", ");
     }
 }
